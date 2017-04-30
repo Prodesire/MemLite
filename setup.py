ea80fc02ed6d1c1d40473a82691439ed3b34c993
Steve Lemuel <wlemuel@hotmail.com>
2017-04-30 15:29:40 +0800

    Add setup.py support for installation
    1. Modify README.rst to add doc related to *installation*
    2. Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="MemLite",
+    version="0.1",
+    packages=find_packages(),
+    author="Prodesire",
+    author_email="Prodesire@yeah.net",
+    description=
+    "a fast, pure-Python, untyped, in-memory database engine, using Python syntax to manage data, instead of SQL, inspired by PyDbLite.",
+    license="MIT",
+    keywords="database",
+    url="https://github.com/Prodesire/MemLite", )
